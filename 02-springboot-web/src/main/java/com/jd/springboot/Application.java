4a8f8f6238d351f1a58ba94873d5bb264ad40784
Fion Yang <yangfei100@jd.com>
2019-08-07 14:30:24 +0800

    第一次上传项目代码

---
+++ 02-springboot-web/src/main/java/com/jd/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.jd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        //启动springboot程序，启动spring容器，启动了内嵌的tomcat
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ 02-springboot-web/src/main/java/com/jd/springboot/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.jd.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: yangfei100
+ * @Date: 2019/8/7
+ * @Description:
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/boot/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+}
